13dd46be0b3030ab09e2f64dfc26b3b4fe1f3cf1
Jean-Éric Bisson <jebisson@ressources.coop>
2026-03-02 22:42:27 -0500

    Hardcode redirectUri production

--- src/authConfig.js
+++ src/authConfig.js
@@ -1,10 +1,8 @@
 export const msalConfig = {
   auth: {
-    clientId: "33d7afc2-f2d7-4b7b-917c-0ca44205a372",           // App Registration → Application (client) ID
+    clientId: "33d7afc2-f2d7-4b7b-917c-0ca44205a372",
     authority: "https://login.microsoftonline.com/e0a16fc3-6382-43ac-ba07-e48f645547ee",
-     redirectUri: process.env.NODE_ENV === "production"
-      ? "https://ashy-smoke-02882c00f.6.azurestaticapps.net"
-      : "http://localhost:3000",  // Ajoutez aussi l'URL de prod ici
+    redirectUri: "https://ashy-smoke-02882c00f.6.azurestaticapps.net",
   },
   cache: {
     cacheLocation: "sessionStorage",
