08149f4429e89042eebab7bf1b0bc954d39c8615
Jean-Éric Bisson <jebisson@ressources.coop>
2026-03-02 22:26:12 -0500

    Fix redirect URI production

--- src/authConfig.js
+++ src/authConfig.js
@@ -2,7 +2,9 @@ export const msalConfig = {
   auth: {
     clientId: "33d7afc2-f2d7-4b7b-917c-0ca44205a372",           // App Registration → Application (client) ID
     authority: "https://login.microsoftonline.com/e0a16fc3-6382-43ac-ba07-e48f645547ee",
-    redirectUri: "http://localhost:3000",  // Ajoutez aussi l'URL de prod ici
+     redirectUri: process.env.NODE_ENV === "production"
+      ? "https://salmon-rock-0a1b2c3d.azurestaticapps.net"
+      : "http://localhost:3000",  // Ajoutez aussi l'URL de prod ici
   },
   cache: {
     cacheLocation: "sessionStorage",
